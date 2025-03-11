cb3567256b09431c7d7d952ac24fbd3b0a4e5ae6
Aleksandr Salnikov <TaynbIy@gmail.com>
2025-03-11 15:31:35 +0500

    cors solve

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,19 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { ConfigService } from '@nestjs/config'
 
 import * as cookieParser from 'cookie-parser'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 
+	const config = app.get(ConfigService)
+
 	app.use(cookieParser())
 	app.enableCors({
-		origin: [process.env.CLIENT_URL],
-		credentials: true,
-		exposedHeaders: 'set-cookie'
+		origin: config.getOrThrow<string>('CLIENT_URL'),
+		credentials: true
+		// exposedHeaders: ['set-cookie']
 	})
 
 	await app.listen(5000)
